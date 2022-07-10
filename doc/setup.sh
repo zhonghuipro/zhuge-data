b3f2703bc715e3bc34621337cec91daa81b993bd
zhonghui <zhonghuipro@qq.com>
2022-07-10 11:44:54 +0800

    add docs

--- doc/setup.sh
+++ doc/setup.sh
@@ -1,2 +1,10 @@
+# 安装 supervisor
+apt-get install -y supervisor
+## 按 supervisor.conf 修改 /etc/supervisor/supervisord.conf
+
+# 安装 nginx
+apt-get install -y nginx
+## 把 nginx.conf 添加到 /etc/nginx/sites-enabled/
+
 # 安装python
 apt install -y python3.8-venv
