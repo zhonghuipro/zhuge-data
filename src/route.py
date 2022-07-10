bba81f06358149ff4f5fafe77978c4cc73313911
zhonghui <zhonghuipro@qq.com>
2022-07-10 12:06:16 +0800

    ai records

---
+++ src/route.py
@@ -0,0 +1,20 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root_request():
+    return "hello, world"
+
+
+@app.route('/call/ai/records')
+def ai_records():
+    return {
+        "code": 0,
+        "msg": "成功"
+    }
+
+
+def route(port):
+    app.run(host='0.0.0.0', port=port)
--- src/start.py
+++ src/start.py
@@ -1,18 +1,10 @@
 import sys
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def root_request():
-    return "hello, world"
-
+from route import route
 
 if __name__ == '__main__':
     print(f"Arguments count: {len(sys.argv)}")
     port = 2022
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
-    app.run(host='0.0.0.0', port=port)
+    route(port)
