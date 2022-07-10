ced59dd4e775363181bab81de0c087009b61309c
zhonghui <zhonghuipro@qq.com>
2022-07-10 12:14:12 +0800

    消息回调

--- src/route.py
+++ src/route.py
@@ -1,15 +1,39 @@
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def root_request():
     return "hello, world"
 
 
-@app.route('/call/ai/records', methods=['POST'])
-def ai_records():
+@app.route('/call/ai/msg/robot', methods=['POST'])
+def robot_msg():
+    return {
+        "code": 0,
+        "msg": "成功"
+    }
+
+
+@app.route('/call/ai/msg/voice', methods=['POST'])
+def voice_msg():
+    return {
+        "code": 0,
+        "msg": "成功"
+    }
+
+
+@app.route('/call/ai/msg/call', methods=['POST'])
+def call_msg():
+    return {
+        "code": 0,
+        "msg": "成功"
+    }
+
+
+@app.route('/call/ai/msg/duration', methods=['POST'])
+def duration_msg():
     return {
         "code": 0,
         "msg": "成功"
