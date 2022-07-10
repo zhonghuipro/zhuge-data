5a10b31d1bf5d3fe3a965288fc8cf4657853681b
zhonghui <zhonghuipro@qq.com>
2022-07-10 12:38:45 +0800

    clean

--- src/start.py
+++ src/start.py
@@ -3,7 +3,6 @@ import sys
 from route import route
 
 if __name__ == '__main__':
-    print(f"Arguments count: {len(sys.argv)}")
     port = 2022
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
