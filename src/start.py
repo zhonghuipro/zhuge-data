a1e9c49f5dd286e4ce4c0f17d180563676567786
zhonghui <zhonghuipro@qq.com>
2022-07-10 10:26:37 +0800

    hello, world

---
+++ src/start.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root_request():
+    return "hello, world"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2022)
